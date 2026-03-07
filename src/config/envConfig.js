83e3bdc08c586adc7a924216d7de55d50fe55207
foisal <foisalrk2@gmail.com>
2026-03-07 17:31:49 +0600

    update url

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -3,13 +3,13 @@
 // export const pdfUrl = "http://10.10.20.57:8001";
 // export const imageUrl = "http://10.10.20.57:8001/uploads";
 
-// export const url = "https://api.profitablebusinessesforsale.com/api/v1/";
-// export const pdfUrl = "https://api.profitablebusinessesforsale.com";
-// export const imageUrl = "https://api.profitablebusinessesforsale.com/uploads";
+export const url = "https://api.profitablebusinessesforsale.com/api/v1/";
+export const pdfUrl = "https://api.profitablebusinessesforsale.com";
+export const imageUrl = "https://api.profitablebusinessesforsale.com/uploads";
 
-export const url = "https://p2zltbm7-8000.inc1.devtunnels.ms/api/v1/";
-export const pdfUrl = "https://p2zltbm7-8000.inc1.devtunnels.ms";
-export const imageUrl = "https://p2zltbm7-8000.inc1.devtunnels.ms/uploads";
+// export const url = "https://p2zltbm7-8000.inc1.devtunnels.ms/api/v1/";
+// export const pdfUrl = "https://p2zltbm7-8000.inc1.devtunnels.ms";
+// export const imageUrl = "https://p2zltbm7-8000.inc1.devtunnels.ms/uploads";
 
 
 
